0eb7c5f6184f6924b464c6ea10b6f6e4f9c38ce4
Sahilpuraiya <112774725+Sahilpuraiya@users.noreply.github.com>
2023-09-13 12:18:01 +0000

    variables

---
+++ 01_basics/01_vari.js
@@ -0,0 +1,12 @@
+const accountId = 1234
+let accountEmail = "sahil@gmail.com"
+var accountphone = 874531
+accountCity = "Indore"
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountphone, accountCity])
+
+accountEmail = "s@s.com"
+accountphone = 84353
+accountCity = "Ujjain"
+console.table([accountEmail, accountId, accountphone, accountCity])
