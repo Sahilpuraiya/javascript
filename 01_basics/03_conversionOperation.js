18e8ee343ada13e7772d1cd7e6d7646842e5c987
Sahilpuraiya <112774725+Sahilpuraiya@users.noreply.github.com>
2023-09-20 20:18:47 +0000

    conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33efe"
+//console.log(score)
+
+//console.log(typeof score)
+
+let valueinnumber = Number(score)
+//console.log(typeof valueinnumber)
+
+//console.log(valueinnumber)
+// "33" => 33
+// "33ads" => NaN
+// true => 1 ; false => 0;
+
+let boolean = 0
+
+let loggedBoolean = Boolean(boolean)
+//console.log(loggedBoolean)
+// "sahil" => true
+// "" => false
+// 1 => true; 0 => false
+
+let number = 33
+let stringConversion = String(number)
+console.log(stringConversion)
+console.log(typeof stringConversion)
+
